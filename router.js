39f057803e8a00dbef74f52cd5a709b12593afab
sebastian oprea <sebastian oprea>
2016-06-04 22:31:48 +0300

    remove "/api" from router

--- router.js
+++ router.js
@@ -2,10 +2,10 @@ const VideoController = require('./controllers/video');
 
 module.exports = function(app) {
 
-  app.post('/api/videos', VideoController.createVideo);
-  app.get('/api/videos', VideoController.fetchVideos);
-  app.get('/api/videos/:id', VideoController.fetchVideo);
-  app.put('/api/videos/:id', VideoController.editVideo);
+  app.post('/videos', VideoController.createVideo);
+  app.get('/videos', VideoController.fetchVideos);
+  app.get('/videos/:id', VideoController.fetchVideo);
+  app.put('/videos/:id', VideoController.editVideo);
 
   app.use(function(req, res){
     res.send(404);
