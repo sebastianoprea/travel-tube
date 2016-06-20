97d4fb22666d83bb194d61fa7feb1190397b35f6
sebastian oprea <sebastian oprea>
2016-06-20 16:32:40 +0300

    update create video and get video

--- apikeys.js
+++ apikeys.js
@@ -1,1 +1,2 @@
 module.exports.GoogleKey = "AIzaSyAW456j7A4w64guSvi1S-MjFFgh29Afnjk";
+module.exports.VimeoKey = "b1ccd11560a24f0ab1a7485b7ae0f086";
--- controllers/video.js
+++ controllers/video.js
@@ -1,6 +1,27 @@
 const Video = require('../models/video');
 const ApiKey = require('../apikeys');
 
+function getThumbnailLink(link, next) {
+  if (link.indexOf("youtube") > -1) {
+      var thumbnail = "  http://img.youtube.com/vi/" + link.slice(link.indexOf("?v=") + 3) + "/0.jpg";
+      next(thumbnail);
+  }
+  else {
+      var id = link.slice(18);
+      var requestUrl = "https://api.vimeo.com/videos/" + id +
+              "/pictures?access_token=" + ApiKey.VimeoKey;
+      var request = require("request");
+      request({
+        url: requestUrl,
+        json: true
+      }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          var thumbnail = body.data[0].sizes[4].link;
+          next(thumbnail);
+        }
+      });
+  }
+}
 function getVideoLocation(searchParam, next) {
   var request = require("request");
   var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + searchParam +
@@ -16,23 +37,25 @@ function getVideoLocation(searchParam, next) {
 }
 exports.createVideo = function(req, res, next) {
   getVideoLocation(req.body.location, function(locationData) {
-    const video = new Video ({
-      title: req.body.title,
-      description: req.body.description,
-      locationX: locationData.geometry.location.lng,
-      locationY: locationData.geometry.location.lat,
-      location: locationData.formatted_address,
-      tags: req.body.tags,
-      created: new Date(),
-      modified: new Date(),
-      thumbnail: req.body.thumbnail,
-      videoUrl: req.body.videoUrl
-    });
-    video.save(function(err) {
-      if (err) {
-        return next(err);
-      }
-      res.json(videos);
+    getThumbnailLink(req.body.videoUrl, function(thumbnail) {
+      const video = new Video ({
+        title: req.body.title,
+        description: req.body.description,
+        locationX: locationData.geometry.location.lng,
+        locationY: locationData.geometry.location.lat,
+        location: locationData.formatted_address,
+        tags: req.body.tags,
+        created: new Date(),
+        modified: new Date(),
+        thumbnail: thumbnail,
+        videoUrl: req.body.videoUrl
+      });
+      video.save(function(err) {
+        if (err) {
+          return next(err);
+        }
+        res.json(video);
+      });
     });
   });
 }
@@ -65,7 +88,7 @@ exports.fetchVideos = function(req, res, next) {
 }
 exports.fetchVideo = function(req, res, next) {
   Video.find({'_id': req.params.id}, function(err, video) {
-    res.json({ video: (video) });
+    res.json(video);
   });
 }
 exports.editVideo =  function(req, res, next) {
